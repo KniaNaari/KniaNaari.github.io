646fff37bc977ebcd161f6dc80ae6caf98e2e9f1
KniaNaari <111276959+KniaNaari@users.noreply.github.com>
2022-08-15 15:02:22 +0530

    multiple webpage informative web

---
+++ HTML/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function()){
+
+$('menu').click(function()){
+
+$(this).toggleclass('fa-times');
+$('.navbar').toggleclass('nav-toggle');
+
+});
+
+$(window).on('load scroll',function(){
+    $('menu').removeclass('fa-times');
+    $('.navbar').toggleclass('nav-toggle');
+}
+
+}
